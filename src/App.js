4f00226c9e3d9ae3f37c223c50a5d8f3ccfd9d0b
Florian Chaux <chaux.florian@gmail.com>
2021-10-11 19:11:45 +0200

    basic level creation in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter, Switch,
+  Route,
+  Link,
+} from 'react-router-dom'
+import EditLevel from './EditLevel';
+import LevelList from './LevelList';
+import PuzzleForm from './PuzzleForm'
+import BasicForm from './BasicForm'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div><ul>
+          <li><Link to="/new/puzzle">Create Puzzle</Link></li>
+          <li><Link to="/new/basic">Create from scratch</Link></li>
+          <li><Link to="/">List</Link></li>
+        </ul></div>
+        <div>
+          <Switch>
+            <Route exact path="/new/puzzle">
+              <PuzzleForm />
+            </Route>
+            <Route exact path="/new/basic">
+              <BasicForm />
+            </Route>
+            <Route path="/edit/:levelId">
+                <EditLevel />
+            </Route>
+            <Route exact path="/">
+              <LevelList />
+            </Route>
+          </Switch>
+        </div>
+
+      </BrowserRouter>
     </div>
   );
 }
